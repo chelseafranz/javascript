e7e79e3c2a0f6f3e62046bf4a0e1129165454620
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-01 18:51:30 -0400

    wed night

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //create empty array to put prices in later
 var priceArray=[];
 
-
-//takes all 'prices' found in array and pushes it top empty array
-var push = items.forEach( function(x){
+//takes all 'prices' found and pushes it up to empty array
+var push = items.forEach(function(x){
   priceArray.push(x.price);
 });
 
@@ -11,32 +10,27 @@ var push = items.forEach( function(x){
 var total= priceArray.reduce(function(a, b){
   return a+b;
 });
-console.log(priceArray);
-console.log(total);
-//divide total or prices by length of array to find average
+
+//divide total of all prices by length of array to find average
 var average= total/priceArray.length;
-  console.log(average);
-  console.log(average.toFixed(2));
+  console.log ("The average price for an item is " + (average.toFixed(2)));
 
 //filters through all objects in var 'items' and returns all with value between 14&18
 var findPrice= items.filter(function(i){
   return i.price > 14 && i.price < 18;
-
 });
-console.log(findPrice);
-
+console.log ("Items that cost between $14 USD and $18 USD: " + (findPrice));
 
 //find all items with a currecy of GBP
 var GBP= items.filter(function(j){
   return j.currency_code==="GBP";
 
 });
-console.log(GBP);
 
 var values= GBP.map.call(GBP, function(v){
-  console.log(v.title + " $" + v.price);
+  console.log(v.title + " costs  \u00A3" + v.price);
 });
-
+//
 items.forEach(function(w){
   w.materials.forEach(function(q){
     if(q.materials === "wood");
@@ -44,17 +38,18 @@ items.forEach(function(w){
   })
 });
 
+//checks all objects called "materials" in 'items' array, only prints the ones that have an array with more that 8 objects inside
 var mat = items.filter(function(m){
-  return m.materials.length > 8;
+  return m.materials.length >= 8;
 });
 console.log(mat);
 
 var howMany = mat.map.call(mat, function(r){
   console.log(r.title + r.materials);
 });
-
+//filters through 'items' to find all cases where object 'who_made' is equal to 'i_did'
 var whoMade= items.filter(function(w){
   return w.who_made === 'i_did';
 });
-
-console.log(whoMade.length);
+//logs the number of objects found in whoMade function
+console.log(whoMade.length) + " were made by their sellers";
