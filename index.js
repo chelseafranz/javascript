b9f5cddb90db9102c8b66501eb898d9e2ded3a7e
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-01 17:12:42 -0400

    wednesday night

--- index.js
+++ index.js
@@ -18,17 +18,7 @@ var average= total/priceArray.length;
   console.log(average);
   console.log(average.toFixed(2));
 
-//filter through all prices and print only those 14-18
-// var t=[];
-// items.map(function(t){
-//     t>=14 && t<=18;
-// console.log(t);
-//  });
-// priceArray.filter(function(a){
-// if (a >=14 && a<=18){
-//   console.log(a);
-// }});
-
+//filters through all objects in var 'items' and returns all with value between 14&18
 var findPrice= items.filter(function(i){
   return i.price > 14 && i.price < 18;
 
@@ -44,5 +34,13 @@ var GBP= items.filter(function(j){
 console.log(GBP);
 
 var values= GBP.map.call(GBP, function(v){
-  console.log(v.title + " " + v.price);
+  console.log(v.title + " $" + v.price);
+});
+
+items.forEach(function(w){
+  w.materials.forEach(function(q){
+    if(q.materials === "wood");
+    console.log(w.title);
+  })
+
 });
