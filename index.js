a5a9eb623ce1202277b96a032000d8705ab8baf1
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-01 15:52:49 -0400

    wednesday afternoon

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 //create empty array to put prices in later
 var priceArray=[];
 
-//checks every item in 'items' array
-items.forEach(function(item){
-  // console.log(item.price);
-});
 
 //takes all 'prices' found in array and pushes it top empty array
 var push = items.forEach( function(x){
@@ -21,3 +17,20 @@ console.log(total);
 var average= total/priceArray.length;
   console.log(average);
   console.log(average.toFixed(2));
+
+//filter through all prices and print only those 14-18
+// var t=[];
+// items.map(function(t){
+//     t>=14 && t<=18;
+// console.log(t);
+//  });
+// priceArray.filter(function(a){
+// if (a >=14 && a<=18){
+//   console.log(a);
+// }});
+
+var findPrice= items.filter(function(i){
+  return i.price > 14 && i.price < 18;
+
+});
+console.log(findPrice);
